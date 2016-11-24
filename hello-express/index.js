7834f421ee26e571902d76561648aaefe09546ec
lucastvasconcelos <lucastavaresvasconcelos@gmail.com>
2016-11-24 19:02:49 -0300

    adicionando porta e envio para navegador em index.js

--- hello-express/index.js
+++ hello-express/index.js
@@ -1,4 +1,14 @@
 // index.js
 const express = require("express");
 
+const app = express();
+
+let teste = "Ola mundo"
+let teste2 = ",hey"
+
+app.get("/hello", (req,res) => res.send(teste));
+
+app.listen(8080);
+
+// to be continued
 // to be continued
